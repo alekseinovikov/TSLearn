f3fce297dcaf8c8bee7d20514e331cc43d3890a0
alekseinovikov <novikov.aleksei.92@gmail.com>
2020-09-04 21:47:47 +0300

    Array functions and object methods

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Ahoy TypeScript!');
+const pizza = {
+  name: "Pepperoni",
+  price: 15,
+  getName() {
+    return this.name;
+  },
+};
+
+const toppings = ["pepperoni"];
+
+function createOrder(pizza: any, toppings: any) {
+  return {
+    pizza,
+    toppings,
+  };
+}
+
+console.log(createOrder(pizza, toppings));
